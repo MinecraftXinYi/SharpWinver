a65b9584d21c608c08e67dde9e03a7d55697feb2
MinecraftXinYi <3077501050@qq.com>
2025-08-03 15:48:45 +0800

    [v3.1.0.2] ...

--- SharpWinver/Core/NativeInterop/RtlHeapAPI.cs
+++ SharpWinver/Core/NativeInterop/RtlHeapAPI.cs
@@ -2,17 +2,17 @@
 
 namespace SharpWinver.Core.NativeInterop;
 
-public unsafe static class RtlHeapApi
+internal unsafe static class RtlHeapApi
 {
     [DllImport(WinDll.NTDLL, SetLastError = true)]
     [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
-    public static extern void* RtlCreateHeap(ulong Flags, void* HeapBase, ulong ReserveSize, ulong CommitSize, void* Lock, void* Parameters);
+    internal static extern void* RtlCreateHeap(ulong Flags, void* HeapBase, ulong ReserveSize, ulong CommitSize, void* Lock, void* Parameters);
 
     [DllImport(WinDll.NTDLL, SetLastError = true)]
     [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
-    public static extern void* RtlAllocateHeap(void* HeapHandle, ulong Flags, ulong Size);
+    internal static extern void* RtlAllocateHeap(void* HeapHandle, ulong Flags, ulong Size);
 
     [DllImport(WinDll.NTDLL, SetLastError = true)]
     [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
-    public static extern void* RtlDestroyHeap(void* HeapHandle);
+    internal static extern void* RtlDestroyHeap(void* HeapHandle);
 }
